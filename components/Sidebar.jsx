9c1e748ddfdbd6503d39fe93a81c14022322c5ab
Teshell <fayrongeek@gmail.com>
2022-07-03 11:40:09 +0100

    search component

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
         alt="logo"
       />
 
-      <div className="text-white">
+      <div className="flex flex-col space-y-8">
         <HomeIcon className="sidebarIcon text-white opacity-[0.85]" />
         <RiCompassFill className="sidebarIcon text-2xl" />
         <FaMicrophoneAlt className="sidebarIcon ml-1" />
